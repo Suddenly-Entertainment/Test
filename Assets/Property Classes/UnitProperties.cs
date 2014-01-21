d1fcb622012a19b4379c10c7a9ecb38e4be75cf2
Spencer Alan Watson <spenceralanwatson@gmail.com>
2014-01-21 14:03:06 -0600

    Added so we can have modularity.
    I added UnitProperties, which extends Properties
    because I wanted it so that any operation on units
    that needs unit related properties, could access
    it from any unit with minimal code.
    This allows them to just cast the properties to this.
    
    Specific Units properties should derive from this.

---
+++ Assets/Property Classes/UnitProperties.cs
@@ -0,0 +1,24 @@
+//  	File Name			:	UnitProperties.cs
+//
+// 	Created By		:	RoboThePichu
+// 	Created On		:	1/21/2014
+//
+// 	Purpose			:	<${Purpose}>
+using System;
+using UnityEngine;
+
+namespace SuddenlyEntertainment
+{
+	public class UnitProperties : Properties
+	{
+		public UnitStats Stats;
+
+		public string ModelName;
+
+		public UnitProperties()
+		{
+
+		}
+	}
+}
+
