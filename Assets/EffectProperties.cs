dfe77ef1b7ecee499e2260b2284df389af9e11ae
Spencer Alan Watson <spenceralanwatson@gmail.com>
2014-01-21 13:43:19 -0600

    This file will house the properties of Effects,
    Effects are explained here: https://github.com/Suddenly-Entertainment/Test/wiki/Effect
    I needed a placeholder so I wouldn't get compiler errors.
    This file completes this by adding in a class, that has nothing in it.

---
+++ Assets/EffectProperties.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SuddenlyEntertainment
+{
+	/// <summary>
+	/// Effect properties.
+	/// </summary>
+	public class EffectProperties : Properties
+	{
+		//TODO: Implement
+	}
+
+}
